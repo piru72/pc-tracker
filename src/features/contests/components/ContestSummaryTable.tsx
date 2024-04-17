6e808c452f5ffb4578f51d58230cf24c31601552
piru72 <parvezdirom2000@gmail.com>
2024-04-17 15:27:28 +0600

    Add : Data generated inside modal

--- src/features/contests/components/ContestSummaryTable.tsx
+++ src/features/contests/components/ContestSummaryTable.tsx
@@ -1,21 +1,32 @@
 import { Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 
-import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
+import { Button, ButtonGroup, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
 import React from 'react';
 import ContestModel from '../../../model/ContestModel';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 interface ContestSummaryTableProps {
     contestData: ContestModel[];
 }
 
+interface TableDataRow {
+    rank: number;
+    teamName: string;
+    totalSolved: number;
+    penalty: number;
+    teamMembers: string[];
+}
 
-const ContestSummaryTable = ({contestData}: ContestSummaryTableProps) => {
+const ContestSummaryTable = ({ contestData }: ContestSummaryTableProps) => {
     const OverlayOne = () => (
         <ModalOverlay
             bg='blackAlpha.300'
             backdropFilter='blur(10px) hue-rotate(0deg)'
         />
     )
+    const [selectedContestDetails, setSelectedContestDetails] = useState<TableDataRow[]>([]);
+    const [selectedContestStandingLink, setSelectedContestStandingLink] = useState<string>('');
     const { isOpen, onOpen, onClose } = useDisclosure()
     const [overlay, setOverlay] = React.useState(<OverlayOne />)
 
@@ -48,6 +59,8 @@ const ContestSummaryTable = ({contestData}: ContestSummaryTableProps) => {
                                         onClick={() => {
                                             setOverlay(<OverlayOne />)
                                             onOpen()
+                                            setSelectedContestDetails(university.data[0].universityTeams)
+                                            setSelectedContestStandingLink(university.data[0].standingLink)
                                         }}> Details</Button></Td>
                                 </Tr>
                             )
@@ -55,30 +68,52 @@ const ContestSummaryTable = ({contestData}: ContestSummaryTableProps) => {
                     }
                     <Modal isCentered isOpen={isOpen} onClose={onClose} size='5xl'>
                         {overlay}
-                        <ModalContent>
+                        <ModalContent id='modal-content'>
                             <ModalHeader textAlign={'center'}>NCPC Onsite JU 2023</ModalHeader>
                             <ModalCloseButton />
                             <ModalBody>
                                 <TableContainer>
-                                    <Table variant='simple'>
+                                    <Table size='lg' variant='simple'>
                                         <TableCaption></TableCaption>
                                         <Thead>
                                             <Tr>
-                                                <Th>Rank</Th>
-                                                <Th>Team Name</Th>
-                                                <Th>Total Solved</Th>
-                                                <Th isNumeric>Penalty</Th>
-                                                <Th isNumeric>Team Members</Th>
+                                                <Th textAlign='center'>Rank</Th>
+                                                <Th textAlign='center'>Team Name</Th>
+                                                <Th textAlign='center'>Total Solved</Th>
+                                                <Th textAlign='center'>Penalty</Th>
+                                                <Th textAlign='center'>Team Members</Th>
                                             </Tr>
                                         </Thead>
                                         <Tbody>
+                                            {selectedContestDetails.map((row, index) => (
+                                                <Tr key={index}>
+                                                    <Td textAlign='center'>{row.rank}</Td>
+                                                    <Td textAlign='center'>{row.teamName}</Td>
+                                                    <Td textAlign='center'>{row.totalSolved}</Td>
+                                                    <Td textAlign='center'>{row.penalty}</Td>
+                                                    <Td textAlign='center'>
+                                                        {row.teamMembers.map((member, memberIndex) => (
+                                                            <React.Fragment key={memberIndex}>
+                                                                {member}
+                                                                {memberIndex !== row.teamMembers.length - 1 && <br />}
+                                                            </React.Fragment>
+                                                        ))}
+                                                    </Td>
+                                                </Tr>
+                                            ))}
                                         </Tbody>
 
                                     </Table>
                                 </TableContainer>
                             </ModalBody>
                             <ModalFooter>
-                                <Button colorScheme='blue'>Full Standing</Button>
+                                <ButtonGroup>
+                                    <Link to={selectedContestStandingLink} target="_blank" rel="noopener noreferrer">
+                                        <Button colorScheme='blue' > Standings </Button>
+                                    </Link>
+
+                                    <Button colorScheme='green'>Download</Button>
+                                </ButtonGroup>
                             </ModalFooter>
                         </ModalContent>
                     </Modal>
