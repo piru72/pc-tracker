6ca3bfe50947f3ad9350dbbebefced3ae0658695
piru72 <parvezdirom2000@gmail.com>
2024-04-19 09:12:57 +0600

    Add : Download feature added

--- src/features/contests/components/ContestSummaryTable.tsx
+++ src/features/contests/components/ContestSummaryTable.tsx
@@ -1,14 +1,36 @@
-import { Button, ButtonGroup, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr, useDisclosure } from '@chakra-ui/react';
+import { Button, ButtonGroup, Modal, ModalContent, ModalFooter, ModalOverlay, Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr, useDisclosure } from '@chakra-ui/react';
+import downloadjs from 'downloadjs';
+import html2canvas from 'html2canvas';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ContestModel from '../../../model/ContestModel';
 import TableDataRow from '../../../model/TableDataRowModel';
 import { formatDate } from '../../../utils/helpers';
 import ModalTablesBody from './ModalTablesBody';
-
 interface ContestSummaryTableProps {
     contestData: ContestModel[];
 }
+const handleCaptureClick = async (imageTitle: string, divName: string) => {
+    const dessiredElement =
+        document.querySelector<HTMLElement>('.'+divName);
+    if (!dessiredElement) return;
+
+    const copiedElement = dessiredElement.cloneNode(
+        true
+    ) as HTMLElement;
+    copiedElement.style.position = 'fixed';
+    copiedElement.style.right = '100%';
+    copiedElement.style.height = 'auto';
+
+    document.body.append(copiedElement);
+
+    const canvas = await html2canvas(copiedElement);
+
+    copiedElement.remove();
+
+    const dataURL = canvas.toDataURL('image/png');
+    downloadjs(dataURL, imageTitle+'.png', 'image/png');
+};
 
 const ContestSummaryTable = ({ contestData }: ContestSummaryTableProps) => {
     const OverlayOne = () => (
@@ -23,7 +45,7 @@ const ContestSummaryTable = ({ contestData }: ContestSummaryTableProps) => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const [overlay, setOverlay] = React.useState(<OverlayOne />)
     const MainTabeHeads = ['Serial', 'Title', 'Event Date', 'Total Participated Teams', 'Top Team Rank', 'AUST Team details']
-    const ModalTableHeads = ['Rank', 'Team Name', 'Total Solved', 'Penalty', 'Team Members']
+    const contestDetailsDiv = 'contest-details-table';
 
     return (
         <TableContainer>
@@ -74,34 +96,20 @@ const ContestSummaryTable = ({ contestData }: ContestSummaryTableProps) => {
 
                     <Modal isCentered isOpen={isOpen} onClose={onClose} size='5xl'>
                         {overlay}
-                        <ModalContent id='modal-content'>
-                            <ModalHeader textAlign={'center'}>{selectedContestTitle}</ModalHeader>
-                            <ModalCloseButton />
-                            <ModalBody>
-                                <TableContainer>
-                                    <Table size='lg' variant='simple'>
-                                        <TableCaption></TableCaption>
-                                        <Thead>
-                                            <Tr>
-                                                {ModalTableHeads.map((head, index) => (
-                                                    <Th key={index} textAlign='center'>{head}</Th>
-                                                ))}
-                                            </Tr>
-                                        </Thead>
-
-                                        <ModalTablesBody tableData={selectedContestDetails} />
-
-
-                                    </Table>
-                                </TableContainer>
-                            </ModalBody>
+                        <ModalContent >
+                            <div className={contestDetailsDiv}>
+                                <ModalTablesBody
+                                    tableData={selectedContestDetails}
+                                    selectedContestTitle={selectedContestTitle}
+                                />
+                            </div>
                             <ModalFooter>
                                 <ButtonGroup>
                                     <Link to={selectedContestStandingLink} target="_blank" rel="noopener noreferrer">
                                         <Button colorScheme='blue' > Full Standings </Button>
                                     </Link>
-
-                                    <Button colorScheme='green'>Download</Button>
+                                    <Button colorScheme='green'
+                                        onClick={() => handleCaptureClick(selectedContestTitle, contestDetailsDiv)}  >Download</Button>
                                 </ButtonGroup>
                             </ModalFooter>
                         </ModalContent>
--- src/features/contests/components/ModalTablesBody.tsx
+++ src/features/contests/components/ModalTablesBody.tsx
@@ -1,32 +1,53 @@
-import { Tbody, Td, Tr } from '@chakra-ui/react';
+import { ModalBody, ModalCloseButton, ModalHeader, Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 import React from 'react';
 import TableDataRow from '../../../model/TableDataRowModel';
 
 interface ModalTablesBodyProps {
     tableData: TableDataRow[];
+    selectedContestTitle: string;
 }
 
-const ModalTablesBody: React.FC<ModalTablesBodyProps> = ({ tableData }) => {
+const ModalTablesBody: React.FC<ModalTablesBodyProps> = ({ tableData, selectedContestTitle }) => {
+    const ModalTableHeads = ['Rank', 'Team Name', 'Total Solved', 'Penalty', 'Team Members']
     return (
-        <Tbody>
-            {tableData.map((row, index) => (
-                <Tr key={index}>
-                    <Td textAlign='center'>{row.rank}</Td>
-                    <Td textAlign='center'>{row.teamName}</Td>
-                    <Td textAlign='center'>{row.totalSolved}</Td>
-                    <Td textAlign='center'>{row.penalty}</Td>
-                    <Td textAlign='center'>
-                        {row.teamMembers.map((member, memberIndex) => (
-                            <React.Fragment key={memberIndex}>
-                                {member || "N/A"}
-                                {memberIndex !== 2 && <br />} {/* Only add <br> for the first two members */}
-                            </React.Fragment>
-                        ))}
-                    </Td>
+        <>
+            <ModalHeader textAlign={'center'}>{selectedContestTitle}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+                <TableContainer>
+                    <Table size='lg' variant='simple'>
+                        <TableCaption></TableCaption>
+                        <Thead>
+                            <Tr>
+                                {ModalTableHeads.map((head, index) => (
+                                    <Th key={index} textAlign='center'>{head}</Th>
+                                ))}
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {tableData.map((row, index) => (
+                                <Tr key={index}>
+                                    <Td textAlign='center'>{row.rank}</Td>
+                                    <Td textAlign='center'>{row.teamName}</Td>
+                                    <Td textAlign='center'>{row.totalSolved}</Td>
+                                    <Td textAlign='center'>{row.penalty}</Td>
+                                    <Td textAlign='center'>
+                                        {row.teamMembers.map((member, memberIndex) => (
+                                            <React.Fragment key={memberIndex}>
+                                                {member || "N/A"}
+                                                {memberIndex !== 2 && <br />} {/* Only add <br> for the first two members */}
+                                            </React.Fragment>
+                                        ))}
+                                    </Td>
 
-                </Tr>
-            ))}
-        </Tbody>
+                                </Tr>
+                            ))}
+                        </Tbody>
+                    </Table>
+                </TableContainer>
+            </ModalBody>
+        </>
+
     );
 };
 
