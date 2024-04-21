534b283afe0882973426978ab831bed7ad1c0ad5
SQL Srijon <116680350+srijon57@users.noreply.github.com>
2024-04-21 12:46:35 +0600

    Update : New data provided in IUPC_DATA and ICPC_DATA file.

--- public/IUPC_DATA.tsx
+++ public/IUPC_DATA.tsx
@@ -55,6 +55,48 @@ const iupc_data: CONTEST_DATA[] = [
                     
 
                 ]
+            },
+            {
+                contestTitle: 'BUET Inter University Programming Contest 2023',
+                contestDate: '2023-07-28',
+                standingLink: 'https://toph.co/c/buet-inter-university-2023/standings',
+                totalParticipatingTeam: 100,
+
+                universityTeams: [
+                    {
+                        rank: 21,
+                        totalSolved: 3,
+                        penalty: 309,
+                        teamName: 'AUST_RedX',
+                        teamMembers: ['NIRJOY DEBNATH', 'SOHOM MUNASIB SAHAUN', 'GOLAM IBNA HAMZA']
+                    },
+
+                    {
+                        rank: 86,
+                        totalSolved: 2,
+                        penalty: 56,
+                        teamName: ' AUST_Badamtola!',
+                        teamMembers: ['APU DAS', 'FATIN ISHRAQ', 'MOHAMMED ABDULLAH-AL SAJID']
+                    },
+                    
+                ]
+            },
+            {
+                contestTitle: 'CEFALO SUST Inter University Programming Contest',
+                contestDate: '2023-01-28',
+                standingLink: 'https://toph.co/c/sust-inter-university-2023/standings/kiosk',
+                totalParticipatingTeam: 50,
+
+                universityTeams: [
+                    {
+                        rank: 28,
+                        totalSolved: 3,
+                        penalty: 507,
+                        teamName: 'AUST_Partisans',
+                        teamMembers: ['NIRJOY DEBNATH', 'SOHOM MUNASIB SAHAUN', 'PARVEZ MIA']
+                    },
+                    
+                ]
             }
         ]
     }
