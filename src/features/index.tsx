f7a4a91c2506f359868fc53d2ff580a31576a1c2
piru72 <parvezdirom2000@gmail.com>
2024-04-30 13:41:55 +0600

    Updatte : Controller introduced

--- src/features/index.tsx
+++ src/features/index.tsx
@@ -7,36 +7,29 @@ const universityShortName = 'AUST';
 const universityFullName = 'Ahsanullah University of Science and Technology';
 const Index = () => {
     const iupc_data = getIUPCData();
-
+    const icpc_data = getICPCData();
+    const ncpc_data = getNCPCData();
+    const contestName = ['IUPC', 'ICPC', 'NCPC']
+    const contestData = [iupc_data, icpc_data, ncpc_data];
     return (
         <Box maxW="10xl" mx={'auto'} pt={5} px={{ base: 2, sm: 12, md: 17 }}>
-            
+
             <SectionTitle title={universityFullName} />
 
             <Tabs isFitted variant='enclosed'>
                 <TabList mb='1em'>
-                    <Tab>IUPC</Tab>
-                    <Tab>ICPC</Tab>
-                    <Tab>NCPC</Tab>
+                    {contestName.map((contest , index)=>
+                    (
+                        <Tab key={index}>{contest}</Tab>)
+                    )}
                 </TabList>
                 <TabPanels>
-                    <TabPanel>
-                        <ContestSummaryTable
-                            contestData={iupc_data}
+                    {contestData.map((contest, index) => (
+                        <TabPanel key={index}>
+                            <ContestSummaryTable contestData={contest}
                             universityShortName={universityShortName} />
-                    </TabPanel>
-                    <TabPanel>
-                        <ContestSummaryTable
-                            contestData={getICPCData()}
-                            universityShortName={universityShortName} />
-
-                    </TabPanel>
-                    <TabPanel>
-                        <ContestSummaryTable
-                            contestData={getNCPCData()}
-                            universityShortName={universityShortName} />
-
-                    </TabPanel>
+                        </TabPanel>
+                    ))}
                 </TabPanels>
             </Tabs>
 
